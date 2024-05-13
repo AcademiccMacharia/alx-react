cb85119fbe02e6f31d7ccc270c0c5af61f8a2e83
AcademiccMacharia <machariab19@gmail.com>
2024-05-13 23:29:35 +0300

    [fix]: restructuring the code into relevant components

---
+++ 0x02-react_props/task_0/dashboard/src/App/App.test.js
@@ -0,0 +1,25 @@
+import React from 'react';
+import { shallow } from 'enzyme';
+import App from './App';
+
+describe('<App />', () => {
+    it('renders without crashing', () => {
+        const wrapper = shallow(<App />);
+        expect(wrapper).toBeDefined();
+    });
+
+    // it('renders a header', () => {
+    //     const wrapper = shallow(<App />);
+    //     expect(wrapper.find('App-header').length).toBeDefined();
+    // });
+
+    // it('renders a body', () => {
+    //     const wrapper = shallow(<App />);
+    //     expect(wrapper.find('App-body').length).toBeDefined();
+    // });
+
+    // it('renders a footer', () => {
+    //     const wrapper = shallow(<App />);
+    //     expect(wrapper.find('App-footer').length).toBeDefined();
+    // });
+})
