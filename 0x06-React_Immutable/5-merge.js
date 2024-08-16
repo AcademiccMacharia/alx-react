73dcad245144dc121654a0a96461e3116aff3e93
AcademiccMacharia <machariab19@gmail.com>
2024-08-17 02:04:30 +0300

    Nested merge

--- 0x06-React_Immutable/5-merge.js
+++ 0x06-React_Immutable/5-merge.js
@@ -1,14 +1,5 @@
-const { List, Map } = require('immutable');
+import { List, Map } from 'immutable';
 
-function concatElements(page1, page2) {
-    return List(page1).concat(List(page2));
-}
+export const concatElements = (page1, page2) => List(page1).concat(List(page2));
 
-function mergeElements(page1, page2) {
-    return Map(page1).merge(Map(page2));
-}
-
-module.exports = {
-    concatElements,
-    mergeElements
-};
+export const mergeElements = (page1, page2) => Map(page1).merge(Map(page2));
